2ae13b958f5cce0f8c1b1cbe77027aaaf4d9ede3
Ismael Castiñeira Paz <a19ismaelcp@iessanclemente.net>
2020-03-26 00:55:54 +0100

    Grandes avances

---
+++ tool_control/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Tool Control",
+    'summary': "Control de herramientas por usuarios",
+    'description': "Aplicacion para el registro de herramientas en un taller comunitario",
+    'author': "Ismael Castiñeira Paz",
+    'website': "https://www.osmeusproxectos.es",
+    'category': 'Extra Tools',
+    'version': '12.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/library_book_categ.xml'
+    ],
+}
--- tool_control/models/library_book.py
+++ tool_control/models/library_book.py
@@ -13,26 +13,25 @@ class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
 
-    name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
-    date_updated = fields.Datetime('Last Updated', copy=False)
-    author_ids = fields.Many2many('res.partner', string='Authors')
-    category_id = fields.Many2one('library.book.category', string='Category')
+    name = fields.Char('Titulo', required=True)
+    date_updated = fields.Datetime('Ultima modificacion', copy=False)
+    category_id = fields.Many2one('library.book.category', string='Categoria')
     state = fields.Selection([
-        ('draft', 'Unavailable'),
-        ('available', 'Available'),
-        ('borrowed', 'Borrowed'),
-        ('lost', 'Lost')],
-        'State', default="draft")
+        ('available', 'Disponible'),
+        ('notavailable', 'Usandose'),
+        ('lost', 'Perdida'),
+        ('broken', 'Rota')],
+        'State', default="available")
 
     @api.model
     def is_allowed_transition(self, old_state, new_state):
-        allowed = [('draft', 'available'),
-                   ('available', 'borrowed'),
-                   ('borrowed', 'available'),
+        allowed = [('available', 'notavailable'),
                    ('available', 'lost'),
-                   ('borrowed', 'lost'),
-                   ('lost', 'available')]
+                   ('notavailable', 'available'),
+                   ('notavailable', 'lost'),
+                   ('notavailable', 'broken'),
+                   ('lost', 'available'),
+                   ('lost', 'broken')]
         return (old_state, new_state) in allowed
 
     @api.multi
@@ -41,38 +40,20 @@ class LibraryBook(models.Model):
             if book.is_allowed_transition(book.state, new_state):
                 book.state = new_state
             else:
-                message = _('Moving from %s to %s is not allowd') % (book.state, new_state)
+                message = _('Pasar de %s a %s no esta permitido') % (book.state, new_state)
                 raise UserError(message)
 
     def make_available(self):
         self.change_state('available')
 
-    def make_borrowed(self):
-        self.change_state('borrowed')
+    def make_notavailable(self):
+        self.change_state('notavailable')
 
     def make_lost(self):
         self.change_state('lost')
 
-    def create_categories(self):
-        categ1 = {
-            'name': 'Child category 1',
-            'description': 'Description for child 1'
-        }
-        categ2 = {
-            'name': 'Child category 2',
-            'description': 'Description for child 2'
-        }
-        parent_category_val = {
-            'name': 'Parent category',
-            'email': 'Description for parent category',
-            'child_ids': [
-                (0, 0, categ1),
-                (0, 0, categ2),
-            ]
-        }
-        # Total 3 records (1 parent and 2 child) will be craeted in library.book.category model
-        record = self.env['library.book.category'].create(parent_category_val)
-        return True
+    def make_broken(self):
+        self.change_state('broken')
 
     @api.multi
     def change_update_date(self):
@@ -102,32 +83,16 @@ class LibraryBook(models.Model):
         filtered_books = self.books_with_multiple_authors(all_books)
         logger.info('Filtered Books: %s', filtered_books)
 
-    @api.model
-    def books_with_multiple_authors(self, all_books):
-        def predicate(book):
-            if len(book.author_ids) > 1:
-                return True
-        return all_books.filtered(predicate)
-
     def mapped_books(self):
         all_books = self.search([])
         books_authors = self.get_author_names(all_books)
         logger.info('Books Authors: %s', books_authors)
-
-    @api.model
-    def get_author_names(self, all_books):
-        return all_books.mapped('author_ids.name')
 
     def sort_books(self):
         all_books = self.search([])
         books_sorted = self.sort_books_by_date(all_books)
         logger.info('Books before sorting: %s', all_books)
         logger.info('Books after sorting: %s', books_sorted)
-
-    @api.model
-    def sort_books_by_date(self, all_books):
-        return all_books.sorted(key='date_release')
-
 
 class LibraryMember(models.Model):
     _name = 'library.member'
